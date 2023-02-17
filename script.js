44203117abd76dba2b90fa6dc25b7948e805cdcb
S1DCO <symoun_ong@dlsu.edu.ph>
2023-02-17 18:03:30 +0800

    can now click and drag to change colors

--- script.js
+++ script.js
@@ -3,18 +3,37 @@
 const gridContainer = document.getElementById("grid-container");
 
 function setUpGrid(size) {
-  gridContainer.setAttribute("grid-template-column", `repeat(${size},1fr)`);
-  gridContainer.setAttribute("grid-template-row", `repeat(${size},1fr)`);
+  gridContainer.style.gridTemplateColumns = `repeat(${size},1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size},1fr)`;
 
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size ** 2; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
     gridContainer.appendChild(gridItem);
   }
 }
 
+//change color on mouse down
+function sketch() {
+  let changeColor = false;
+  document.addEventListener("mousedown", function (e) {
+    changeColor = true;
+    if (!(e.target === e.target.closest(".grid-item"))) return;
+    e.target.classList.add("grid-item-active");
+  });
+
+  gridContainer.addEventListener("mouseover", function (e) {
+    if (!(e.target === e.target.closest(".grid-item"))) return;
+    if (!changeColor) return;
+    e.target.classList.add("grid-item-active");
+  });
+  document.addEventListener("mouseup", function () {
+    changeColor = false;
+  });
+}
 function clearGrid() {
   gridContainer.innerHTML = "";
 }
 
 setUpGrid(8);
+sketch();
