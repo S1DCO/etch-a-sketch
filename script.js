43b6c0334ecfb58c10f9441fc233fb37b61dc36e
S1DCO <symoun_ong@dlsu.edu.ph>
2023-02-18 18:56:03 +0800

    I'm bored

--- script.js
+++ script.js
@@ -3,13 +3,17 @@
 const gridContainer = document.getElementById("grid-container");
 const btnReset = document.getElementById("btn-reset");
 const btnColor = document.getElementById("btn-color");
+const gridSize = document.getElementById("grid--size");
+const gridSizeLabel = document.querySelector(".label-grid--size");
 
 let currentColor = btnColor.value;
 let changeColor = false;
 let gridColor = btnColor.value;
 
-console.log(gridColor);
-function setUpGrid(size) {
+function setUpGrid() {
+  gridContainer.innerHTML = "";
+  let size = gridSize.value;
+  gridSizeLabel.textContent = `${size}x${size}`;
   gridContainer.style.gridTemplateColumns = `repeat(${size},1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${size},1fr)`;
 
@@ -23,35 +27,45 @@ function setUpGrid(size) {
 function colorOnMouseDown(e) {
   changeColor = true;
   if (!(e.target === e.target.closest(".grid-item"))) return;
-  e.target.classList.add("grid-item-colored");
+  e.target.style.backgroundColor = currentColor;
 }
 function colorOnMouseOver(e) {
   if (!(e.target === e.target.closest(".grid-item"))) return;
   if (!changeColor) return;
-  e.target.classList.add("grid-item-colored");
+  e.target.style.backgroundColor = currentColor;
 }
 
 //change color on mouse down
 function addColorToGrid() {
   //change color when mouse is down
-  document.addEventListener("mousedown", colorOnMouseDown);
+  gridContainer.addEventListener("mousedown", colorOnMouseDown);
 
   gridContainer.addEventListener("mouseover", colorOnMouseOver);
   //when mouse is up cannot change color
-  document.addEventListener("mouseup", () => {
+  gridContainer.addEventListener("mouseup", () => {
+    changeColor = false;
+  });
+  document.addEventListener("mouseover", (e) => {
+    if (e.target === e.target.closest(".grid-item")) return;
     changeColor = false;
   });
 }
 
+// --------button--------
 function clearGrid() {
   const allGridItems = document.querySelectorAll(".grid-item");
-  allGridItems.forEach((el) => el.classList.remove("grid-item-colored"));
+  allGridItems.forEach((el) => (el.style.backgroundColor = "white"));
 }
 
+function changeCurrentColor() {
+  currentColor = btnColor.value;
+}
 //only function to be run outside a function
 function run() {
-  setUpGrid(100);
+  setUpGrid();
+  gridSize.addEventListener("change", setUpGrid);
   addColorToGrid();
   btnReset.addEventListener("click", clearGrid);
+  btnColor.addEventListener("change", changeCurrentColor);
 }
 run();
